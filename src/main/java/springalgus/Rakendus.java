f9379bcead12c4cf05865503b9a52dc251725686
zeus <zeus@A406-0.INFORMAATIKA>
2018-03-08 10:57:38 +0200

    Summa

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -23,6 +23,19 @@ public class Rakendus {
     }
     //http://localhost:8080/korrutus?arv1=3&arv2=5
  
+    @RequestMapping("/liidaArvud")
+    public int arvudeLiitmine(String sisu){
+        String[] m=sisu.split(",");
+        int summa=0; 
+        for(int i=0; i<m.length; i++){
+           summa+=Integer.parseInt(m[i].trim());
+        }
+        //return summa;
+        
+        return java.util.Arrays.stream(sisu.split(",")).
+                 mapToInt(s -> Integer.parseInt(s.trim())).sum(); 
+    }
+    //http://localhost:8080/liidaArvud?sisu=1,10,200
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 40305);
         SpringApplication.run(Rakendus.class, args);
